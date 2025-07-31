2fd9adf8e4cf402afb84466fb79d811860d1da83
keloffic <businesscandioti@gmail.com>
2025-07-31 10:00:59 -0500

    Delete src directory

--- src/logger.ts
+++
@@ -1,9 +0,0 @@
-import consola from "consola";
-import config from "./config.ts";
-
-// Configure consola logger
-const logger = consola.create({
-  level: config.LOG_LEVEL || 4, // 4 = info level, 5 = debug level
-});
-
-export default logger;
--- src/transcription.ts
+++
@@ -1,2 +0,0 @@
-// Re-export TranscriptionService from the new file
-export { TranscriptionService } from "./transcription-service.ts";
